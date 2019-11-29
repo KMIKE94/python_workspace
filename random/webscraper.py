b51f1a528bcf2895f5a713b759bfe22fbb8e0a37
KMIKE94 <w.keith.2013@outlook.com>
2019-11-29 12:27:00 -0500

    able to scrape the journal for headlines and links

--- random/webscraper.py
+++ random/webscraper.py
@@ -1,7 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 response = requests.get('http://thejournal.ie')
+soup = BeautifulSoup(response.text, "html.parser")
+h4 = soup.find_all("h4")
 
-print(response.content)
-print("<<<<")
-print(response.text)
+print("<<<")
+for h in h4:
+    print(h.a.string + "\t" + h.a.get('href'))
+print(">>>")
