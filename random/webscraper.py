67d38fc0cac8185b6097e3ec695c12d8a8ad9539
KMIKE94 <w.keith.2013@outlook.com>
2019-11-29 16:48:16 -0500

    trying to add list of h2 tags with values and anchor tags

--- random/webscraper.py
+++ random/webscraper.py
@@ -4,8 +4,13 @@ from bs4 import BeautifulSoup
 response = requests.get('http://thejournal.ie')
 soup = BeautifulSoup(response.text, "html.parser")
 h4 = soup.find_all("h4")
+my_soup = BeautifulSoup("<html></html>")
+html = my_soup.html
 
 print("<<<")
 for h in h4:
     print(h.a.string + "\t" + h.a.get('href'))
+    h2 = my_soup.new_tag("h2")
+    h2.string = h.a.string
+    a = my_soup.new_tag("a", href=h.a.get('href'))
 print(">>>")
