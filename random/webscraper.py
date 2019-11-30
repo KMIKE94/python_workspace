0199c63cb3d48ded32cbd32ff9438a17361a8e37
KMIKE94 <w.keith.2013@outlook.com>
2019-11-30 15:36:32 -0500

    abstracting results to an object

--- random/webscraper.py
+++ random/webscraper.py
@@ -1,16 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('http://thejournal.ie')
-soup = BeautifulSoup(response.text, "html.parser")
-h4 = soup.find_all("h4")
-my_soup = BeautifulSoup("<html></html>")
-html = my_soup.html
+class Results:
+    def __init__(self, title, link):
+        self.title = title
+        self.link = link
 
-print("<<<")
-for h in h4:
-    print(h.a.string + "\t" + h.a.get('href'))
-    h2 = my_soup.new_tag("h2")
-    h2.string = h.a.string
-    a = my_soup.new_tag("a", href=h.a.get('href'))
-print(">>>")
+    def getTitle(self):
+        return self.title
+    
+    def getLink(self):
+        return self.link
+
+
+def getResults():
+    response = requests.get('http://thejournal.ie')
+    soup = BeautifulSoup(response.text, "html.parser")
+    h4 = soup.find_all("h4")
+    
+    myResults = [Results(h_tag.a.string,h_tag.a.get('href')) for h_tag in h4]
+
+    print("<<<")
+    # for h in h4:
+    #     print(h.a.string + "\t" + h.a.get('href'))
+
+    for a in myResults:
+        print(a.getTitle() + "\t" + a.getLink())
+
+    print(">>>")
+
+if __name__ == "__main__":
+    getResults()
