aaad3a098f519cb874c4433049cc425d479e0469
KMIKE94 <w.keith.2013@outlook.com>
2019-12-03 16:24:34 -0500

    Adding a test document of div tags containing comments and views

--- random/webscraper.py
+++ random/webscraper.py
@@ -16,18 +16,16 @@ class Results:
 def getResults():
     response = requests.get('http://thejournal.ie')
     soup = BeautifulSoup(response.text, "html.parser")
-    h4 = soup.find_all("h4")
-    
-    myResults = [Results(h_tag.a.string,h_tag.a.get('href')) for h_tag in h4]
+    h4 = soup.find_all("div")
+    for h in h4:
+        print(h)
 
-    print("<<<")
-    # for h in h4:
-    #     print(h.a.string + "\t" + h.a.get('href'))
+    # myResults = [Results(h_tag.a.string,h_tag.a.get('href')) for h_tag in h4]
 
-    for a in myResults:
-        print(a.getTitle() + "\t" + a.getLink())
-
-    print(">>>")
+    # print("<<<")
+    # for a in myResults:
+        # print(a.getTitle() + "\t" + a.getLink())
+    # print(">>>")
 
 if __name__ == "__main__":
     getResults()
