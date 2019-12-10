7c849644912748a1eaa593de57de1207d2514360
KMIKE94 <w.keith.2013@outlook.com>
2019-12-10 14:18:34 -0500

    Updating example requests code

--- requests/callSpringApp.py
+++ requests/callSpringApp.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
+# Accepts a URL and will perform a get request
+# grep the response to find data
+
 if __name__ == "__main__":
     print("Enter the URL",end=": ")
     url = str(input())
     jsonResponse = requests.get(url)
     print(jsonResponse.json())
 
-    customer = json.loads(jsonResponse.text)
-    print(customer["firstName"])
+    return json.loads(jsonResponse.text)
 
 
